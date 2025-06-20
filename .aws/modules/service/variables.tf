b091a6d3a4caba86c230751e6a37ca7d359a06e1
losnfx <losnfxcode@gmail.com>
2025-06-19 21:40:18 -0600

    test if work

--- .aws/modules/service/variables.tf
+++ .aws/modules/service/variables.tf
@@ -1,4 +1,15 @@
-# variables.tf
-variable "env" {}
-variable "region" {}
-variable "bucket_name" {}
+
+variable "env" {
+  description = "The deployment environment (e.g., dev, staging, prod)"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region to deploy the service"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket used by the service"
+  type        = string
+}
--- .aws/workspaces/services/main.tf
+++ .aws/workspaces/services/main.tf
@@ -4,3 +4,18 @@ module "web" {
   region      = var.region
   bucket_name = var.bucket_name
 }
+
+variable "env" {
+  description = "The deployment environment (e.g., dev, staging, prod)"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region to deploy the service"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket used by the service"
+  type        = string
+}
