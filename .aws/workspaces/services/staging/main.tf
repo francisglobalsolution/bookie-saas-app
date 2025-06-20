aaf156cf7ef1341d1e0bed8a23b05c5564403850
losnfx <losnfxcode@gmail.com>
2025-06-20 01:11:09 -0600

    fix main

--- .aws/workspaces/services/staging/main.tf
+++ .aws/workspaces/services/staging/main.tf
@@ -2,7 +2,7 @@
 // Add your Terraform resources here
 
 module "web" {
-  source      = "../../modules/service"
+  source      = "../../../modules/service"
   env         = var.env
   region      = var.region
   bucket_name = var.bucket_name
