6d47c2aca80243252031e44de4905b9ede32d148
losnfx <losnfxcode@gmail.com>
2025-06-20 15:43:41 -0600

    fix cicd

--- .aws/workspaces/services/backend.tf
+++ .aws/workspaces/services/backend.tf
@@ -1,3 +1,10 @@
 terraform {
-  backend "remote" {}
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "Francis_Global_Solution"
+
+    workspaces {
+      prefix = "bookie-"
+    }
+  }
 }
