184fcdea82906ac8d764404ee14ea765d85d8e04
losnfx <losnfxcode@gmail.com>
2025-06-20 18:57:05 -0600

    add variabe to main

--- .aws/workspaces/services/main.tf
+++ .aws/workspaces/services/main.tf
@@ -5,3 +5,25 @@ module "web" {
   bucket_name     = aws_s3_bucket.service_bucket.bucket
   distribution_id = aws_cloudfront_distribution.site.id
 }
+
+
+
+variable "env" {
+  description = "The deployment environment (e.g., dev, staging, prod)"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region to deploy the service"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket used by the service"
+  type        = string
+}
+
+variable "distribution_id" {
+  description = "CloudFront Distribution ID"
+  type        = string
+}
