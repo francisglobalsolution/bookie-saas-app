0495f1cd0bd582056b3e21adcd951d6c219a67a3
losnfx <losnfxcode@gmail.com>
2025-06-20 19:12:06 -0600

    update output

--- .aws/modules/service/outputs.tf
+++ .aws/modules/service/outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_name" {
-  value = var.bucket_name
+  value = aws_s3_bucket.service_bucket.bucket
 }
 
 output "distribution_id" {
-  value = var.distribution_id
+  value = aws_cloudfront_distribution.site.id
 }
