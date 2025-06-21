6329042d9511eb994a5c7d79d0bbe2b95db7899e
losnfx <losnfxcode@gmail.com>
2025-06-20 18:47:56 -0600

    update variable

--- .aws/modules/service/variables.tf
+++ .aws/modules/service/variables.tf
@@ -13,3 +13,8 @@ variable "bucket_name" {
   description = "Name of the S3 bucket used by the service"
   type        = string
 }
+
+variable "distribution_id" {
+  description = "CloudFront Distribution ID"
+  type        = string
+}
