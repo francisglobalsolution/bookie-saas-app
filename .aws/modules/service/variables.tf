2e75f042d8de5eabd06a9ec6330992dbd489d5ec
losnfx <losnfxcode@gmail.com>
2025-06-20 19:07:54 -0600

    fix variable

--- .aws/modules/service/variables.tf
+++ .aws/modules/service/variables.tf
@@ -8,16 +8,6 @@ variable "region" {
   type        = string
 }
 
-variable "bucket_name" {
-  description = "Name of the S3 bucket used by the service"
-  type        = string
-}
-
-variable "distribution_id" {
-  description = "CloudFront Distribution ID"
-  type        = string
-}
-
 variable "bucket_name_prefix" {
   description = "Prefix for the S3 bucket name"
   type        = string
