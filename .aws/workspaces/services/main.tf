d4dbc8f3fafa5361ef9713d0fe84117f14d06923
losnfx <losnfxcode@gmail.com>
2025-06-20 18:40:36 -0600

    remove dupllicate variable

--- .aws/workspaces/services/main.tf
+++ .aws/workspaces/services/main.tf
@@ -5,18 +5,3 @@ module "web" {
   bucket_name     = aws_s3_bucket.service_bucket.bucket
   distribution_id = aws_cloudfront_distribution.site.id
 }
-
-variable "env" {
-  description = "The deployment environment (e.g., dev, staging, prod)"
-  type        = string
-}
-
-variable "region" {
-  description = "AWS region to deploy the service"
-  type        = string
-}
-
-variable "bucket_name" {
-  description = "Name of the S3 bucket used by the service"
-  type        = string
-}
