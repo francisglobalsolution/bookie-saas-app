0b1287e544eda1885f0b5124797f7a0b5a436979
kevralfrancis <kevralfrancis29@gmail.com>
2025-09-11 01:25:03 -0400

    fix tf error

--- .aws/workspaces/services/prod/main.tf
+++ .aws/workspaces/services/prod/main.tf
@@ -1,15 +1,24 @@
 terraform {
   required_version = ">= 1.3.0"
+
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
+
   cloud {
     organization = "Francis_Global_Solution"
-    workspaces { name = "bookie-<env>" } # dev | staging | qa | prod
+    workspaces {
+      name = "bookie-prod"
+    }
   }
 }
 
-provider "aws" { region = var.region }
+provider "aws" {
+  region = var.region
+}
 
 module "web" {
   source             = "../../../modules/service"
@@ -20,4 +29,3 @@ module "web" {
   force_destroy      = var.force_destroy
   tags               = var.tags
 }
-
