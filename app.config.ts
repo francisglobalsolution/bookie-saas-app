a2b745e9a9a0ed696138239a700106ae52dd1001
kevralfrancis <kevralfrancis29@gmail.com>
2025-09-16 22:30:52 -0400

    fix prettier problem

--- app.config.ts
+++ app.config.ts
@@ -8,28 +8,28 @@ export default ({ config }: ConfigContext): ExpoConfig => {
   const profile = process.env.EAS_BUILD_PROFILE || "dev";
 
   const name = pick(profile, {
-    dev:     "Bookie (Dev)",
+    dev: "Bookie (Dev)",
     staging: "Bookie (Staging)",
-    qa:      "Bookie (QA)",
-    prod:    "Bookie",
+    qa: "Bookie (QA)",
+    prod: "Bookie",
   });
 
   const slug = "bookie-saas-app-native";
 
   const iosBundleId = pick(profile, {
-    dev:     "com.bookie.app.dev",
+    dev: "com.bookie.app.dev",
     staging: "com.bookie.app.staging",
-    qa:      "com.bookie.app.qa",
-    prod:    "com.bookie.app",
+    qa: "com.bookie.app.qa",
+    prod: "com.bookie.app",
   });
 
   const androidPackage = iosBundleId;
 
   const channel = pick(profile, {
-    dev:     "dev",
+    dev: "dev",
     staging: "staging",
-    qa:      "qa",
-    prod:    "prod",
+    qa: "qa",
+    prod: "prod",
   });
 
   return {
@@ -53,7 +53,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     extra: {
       API_URL: process.env.API_URL,
       SENTRY_DSN: process.env.SENTRY_DSN,
-      USE_MOCKS: process.env.USE_MOCKS,  
+      USE_MOCKS: process.env.USE_MOCKS,
       eas: { projectId: "9bc3ca10-c107-46d8-b3af-4154936acce3" },
     },
     updatesChannel: channel,
